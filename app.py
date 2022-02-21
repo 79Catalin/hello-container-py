feb8faf360b06552b9d7107374815ee356b76a9d
79Catalin <91431727+79Catalin@users.noreply.github.com>
2022-02-21 21:19:01 +0200

    Update app.py
    2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from inside the GitHub Pull Request!"
+    return "Hello World, from inside the GitHub Pull Request 2!"
 
 
 if __name__ == "__main__":
